d3f3940ec6f37ef43b480899c95b90c3e4e01137
XanderKing <kenneth.lam1997@hotmail.com>
2024-04-24 05:06:43 -0400

    Added minimize and maximize button functionality to windows

--- Assets/Scripts/Window.cs
+++ Assets/Scripts/Window.cs
@@ -14,6 +14,12 @@ public class Window : MonoBehaviour {
         header = transform.Find("Header").gameObject;
         body = transform.Find("Body").gameObject;
 
+        Button minimizeButton = header.transform.Find("Minimize Button").GetComponent<Button>();
+        minimizeButton.onClick.AddListener(MinimizeWindow);
+
+        Button maximizeButton = header.transform.Find("Maximize Button").GetComponent<Button>();
+        maximizeButton.onClick.AddListener(MaximizeWindow);
+
         Button closeButton = header.transform.Find("Close Button").GetComponent<Button>();
         closeButton.onClick.AddListener(CloseWindow);
     }
@@ -39,6 +45,32 @@ public class Window : MonoBehaviour {
     protected string ToCapitalize(string x)
     {
         return Regex.Replace(x, @"\b([a-z])", m => m.Value.ToUpper());
+    }
+
+    protected void MinimizeWindow()
+    {
+        body.SetActive(false);
+        CanvasGroup minimizeCanvas = header.transform.Find("Minimize Button").GetComponent<CanvasGroup>();
+        minimizeCanvas.alpha = 0;
+        minimizeCanvas.interactable = false;
+        minimizeCanvas.blocksRaycasts = false;
+        CanvasGroup maximizeCanvas = header.transform.Find("Maximize Button").GetComponent<CanvasGroup>();
+        maximizeCanvas.alpha = 1;
+        maximizeCanvas.interactable = true;
+        maximizeCanvas.blocksRaycasts = true;
+    }
+
+    protected void MaximizeWindow()
+    {
+        body.SetActive(true);
+        CanvasGroup minimizeCanvas = header.transform.Find("Minimize Button").GetComponent<CanvasGroup>();
+        minimizeCanvas.alpha = 1;
+        minimizeCanvas.interactable = true;
+        minimizeCanvas.blocksRaycasts = true;
+        CanvasGroup maximizeCanvas = header.transform.Find("Maximize Button").GetComponent<CanvasGroup>();
+        maximizeCanvas.alpha = 0;
+        maximizeCanvas.interactable = false;
+        maximizeCanvas.blocksRaycasts = false;
     }
 
     protected void CloseWindow()
