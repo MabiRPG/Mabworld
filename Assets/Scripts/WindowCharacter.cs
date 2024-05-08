1bc37214ced25829b7882e2025c405cc706b339a
ktthai <kethtran@gmail.com>
2024-05-07 23:02:28 -0700

    Added defensive stats to Window Character

--- Assets/Scripts/WindowCharacter.cs
+++ Assets/Scripts/WindowCharacter.cs
@@ -5,12 +5,21 @@ public class WindowCharacter : Window
 {
     public static WindowCharacter Instance = null;
 
+    // HP and MP
     private ProgressBar actorHPBar;
     private ProgressBar actorMPBar;
+
+    // Regular Stats
     private TMP_Text actorStrText;
     private TMP_Text actorIntText;
     private TMP_Text actorDexText;
     private TMP_Text actorLuckText;
+
+    // Defensive Stats
+    private TMP_Text actorDefenseText;
+    private TMP_Text actorProtectionText;
+    private TMP_Text actorMagicDefenseText;
+    private TMP_Text actorMagicProtectionText;
 
     protected override void Awake()
     {
@@ -30,12 +39,22 @@ public class WindowCharacter : Window
         gameObject.SetActive(false);
 
         GameObject character = body.transform.Find("Character").gameObject;
+
+        // HP and MP
         actorHPBar = character.transform.Find("HP & MP Bar Parent").Find("HP Bar Parent").Find("HP Bar").GetComponent<ProgressBar>();
         actorMPBar = character.transform.Find("HP & MP Bar Parent").Find("MP Bar Parent").Find("MP Bar").GetComponent<ProgressBar>();
+
+        // Regular Stats
         actorStrText = character.transform.Find("Stats Parent").Find("Str Parent").Find("Value").GetComponent<TMP_Text>();
         actorIntText = character.transform.Find("Stats Parent").Find("Int Parent").Find("Value").GetComponent<TMP_Text>();
         actorDexText = character.transform.Find("Stats Parent").Find("Dex Parent").Find("Value").GetComponent<TMP_Text>();
         actorLuckText = character.transform.Find("Stats Parent").Find("Luck Parent").Find("Value").GetComponent<TMP_Text>();
+
+        // Defensive Stats
+        actorDefenseText = character.transform.Find("Defensive Stats Parent").Find("Defense Parent").Find("Value").GetComponent<TMP_Text>();
+        actorProtectionText = character.transform.Find("Defensive Stats Parent").Find("Protection Parent").Find("Value").GetComponent<TMP_Text>();
+        actorMagicDefenseText = character.transform.Find("Defensive Stats Parent").Find("Magic Defense Parent").Find("Value").GetComponent<TMP_Text>();
+        actorMagicProtectionText = character.transform.Find("Defensive Stats Parent").Find("Magic Protection Parent").Find("Value").GetComponent<TMP_Text>();
     }
 
     private void Start()
@@ -45,13 +64,22 @@ public class WindowCharacter : Window
 
     private void Draw()
     {
+        // HP and MP
         actorHPBar.SetCurrent(Player.Instance.actorHP.current);
         actorHPBar.SetMaximum(Player.Instance.actorHP.maximum);
         actorMPBar.SetCurrent(Player.Instance.actorMP.current);
         actorMPBar.SetMaximum(Player.Instance.actorMP.maximum);
+
+        // Regular Stats
         actorStrText.text = Player.Instance.actorStr.current.ToString();
         actorIntText.text = Player.Instance.actorInt.current.ToString();
         actorDexText.text = Player.Instance.actorDex.current.ToString();
         actorLuckText.text = Player.Instance.actorLuck.current.ToString();
+
+        // Defensive Stats
+        actorDefenseText.text = Player.Instance.actorDefense.current.ToString();
+        actorProtectionText.text = Player.Instance.actorProtection.current.ToString();
+        actorMagicDefenseText.text = Player.Instance.actorMagicDefense.current.ToString();
+        actorMagicProtectionText.text = Player.Instance.actorMagicProtection.current.ToString();
     }
 }
