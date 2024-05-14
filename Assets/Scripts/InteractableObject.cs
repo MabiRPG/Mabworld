0cb2a83e9d6044ef5055383bba8508f5effe775d
XanderKing <kenneth.lam1997@hotmail.com>
2024-05-13 20:51:17 -0400

    Revert "Revert "Initial database and integration rework""
    This reverts commit 6ecf70acbc1bb06207ccaa1b099ecca89b4a12fd.

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,25 +1,47 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    public int eventID;
-    public Dictionary<string, object> info = new Dictionary<string, object>();
+    // Primary key for event
+    public int ID;
+    // Skill associated
+    public int skillID;
+    // Rank restrictions on skill to gather
+    public string rankRequired;
+    public float successRateModifier;
+    // Label to display in world
+    public string name;
+    // Sprites to display depending on state of resource
+    public Sprite fullSprite;
+    public Sprite partialFullSprite;
+    public Sprite emptySprite;
+    // Mouse hover sprite
+    public Sprite mouseHoverSprite;
+    // Sound effect when interacting
+    public AudioClip sfx;
+    // How much the resource currently has
+    public ValueManager resource;
+    // Maximum capacity of resource
+    public int resourceMax;
+    // How much it regenerates per interval, and interval duration
+    public int resourceRegenPerInterval;
+    public int resourceRegenInterval;
+    // What loot table this resource draws from
+    public int lootTableID;
+    public bool isMovable;
 
     private const string eventQuery = @"SELECT * FROM map_resources WHERE event_id = @id LIMIT 1;";
 
     private void Start()
     {
-        DataTable dt = GameManager.Instance.QueryDatabase(eventQuery, ("@id", eventID));
+        DataTable dt = GameManager.Instance.QueryDatabase(eventQuery, ("@id", ID));
+        DataRow row = dt.Rows[0];
 
-        foreach (DataRow row in dt.Rows)
-        {
-            foreach (DataColumn column in dt.Columns)
-            {
-                info.Add(column.ColumnName, row[column]);
-            }
-        }
+        ID = (int)row["event_id"];
+        skillID = (int)row["skill_id"];
 
         dt.Clear();
     }
