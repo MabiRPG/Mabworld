8a6354f353964dffcec6b5f434e72c1fa8eb0b38
XanderKing <kenneth.lam1997@hotmail.com>
2024-09-29 16:32:26 -0400

    Updated skill stat tree view to multi column view, hooked up all ranks to window.

--- Assets/EditorWindows/SimpleEditorWindow.cs
+++ Assets/EditorWindows/SimpleEditorWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 using UnityEditor;
@@ -87,127 +88,158 @@ public class SimpleEditorWindow : EditorWindow
         leftPane.columns["name"].bindCell = (item, index) => { (item as Label).text = skills[index].name; };
         
         leftPane.selectedIndicesChanged += OnSkillSelectionChange;
+
+        MultiColumnListView statView = rootVisualElement.Q<MultiColumnListView>("selectedStats");
+        statView.columns["stat"].makeCell = () => new Label();
+        statView.columns["stat"].bindCell = (item, j) => {
+            foreach (SkillStatTypeModel statType in skillStatTypes)
+            {
+                if (statType.ID == (statView.itemsSource[j] as SkillStatModel).statID)
+                {
+                    string statName = statType.name.Replace("_", " ");
+                    (item as Label).text = statName;
+                    return;
+                }
+            }
+        };
+
+        for (int i = 15; i > 0; i--)
+        {
+            int j = i; // For local purposes.
+            string hex = j.ToString("X");
+
+            statView.columns[hex].makeCell = () => new Label();
+            statView.columns[hex].bindCell = (item, k) => {
+                (item as Label).text = (statView.itemsSource[k] as SkillStatModel).values[j - 1].ToString(); 
+            };
+        }        
     }
 
     private void OnSkillSelectionChange(IEnumerable<int> selectedIndex)
     {
         var enumerator = selectedIndex.GetEnumerator();
 
-        if (enumerator.MoveNext())
+        if (!enumerator.MoveNext())
         {
-            int index = enumerator.Current;
-            Button selectedIcon = rootVisualElement.Q<Button>("selectedIcon");
-            selectedIcon.style.backgroundImage = new StyleBackground(skills[index].icon);
-            //selectedIcon.clicked += ChangeIcon;
-
-            TextField selectedName = rootVisualElement.Q<TextField>("selectedName");
-            selectedName.value = skills[index].name;
-
-            DropdownField selectedCategory = rootVisualElement.Q<DropdownField>("selectedCategory");
-            List<string> names = new List<string>();
-
-            foreach (SkillTypeModel category in skillTypes)
-            {
-                names.Add(category.name);
-
-                if (category.ID == skills[index].categoryID)
-                {
-                    selectedCategory.value = category.name;
-                }
-            }
-
-            selectedCategory.choices = names;
-
-            TextField selectedDescription = rootVisualElement.Q<TextField>("selectedDescription");
-            selectedDescription.value = skills[index].description;
-
-            TextField selectedDetails = rootVisualElement.Q<TextField>("selectedDetails");
-            selectedDetails.value = skills[index].details;
-
-            DropdownField selectedFirstRank = rootVisualElement.Q<DropdownField>("selectedFirstRank");
-            selectedFirstRank.value = skills[index].firstAvailableRank;
-            selectedFirstRank.choices = SkillModel.ranks;
-
-            DropdownField selectedStartRank = rootVisualElement.Q<DropdownField>("selectedStartRank");
-            selectedStartRank.value = skills[index].startingRank;
-            selectedStartRank.choices = SkillModel.ranks;       
-            
-            DropdownField selectedLastRank = rootVisualElement.Q<DropdownField>("selectedLastRank");
-            selectedLastRank.value = skills[index].lastAvailableRank;
-            selectedLastRank.choices = SkillModel.ranks;
-
-            FloatField selectedBaseLoadTime = rootVisualElement.Q<FloatField>("selectedBaseLoadTime");
-            selectedBaseLoadTime.value = skills[index].baseLoadTime;           
-
-            FloatField selectedBaseUseTime = rootVisualElement.Q<FloatField>("selectedBaseUseTime");
-            selectedBaseUseTime.value = skills[index].baseLoadTime;    
-
-            FloatField selectedBaseCooldownTime = rootVisualElement.Q<FloatField>("selectedBaseCooldownTime");
-            selectedBaseCooldownTime.value = skills[index].baseLoadTime;
-
-            Toggle selectedIsStartingWith = rootVisualElement.Q<Toggle>("selectedIsStartingWith");
-            selectedIsStartingWith.value = skills[index].isStartingWith; 
-
-            Toggle selectedIsLearnable = rootVisualElement.Q<Toggle>("selectedIsLearnable");
-            selectedIsLearnable.value = skills[index].isLearnable; 
-
-            Toggle selectedIsPassive = rootVisualElement.Q<Toggle>("selectedIsPassive");
-            selectedIsPassive.value = skills[index].isPassive;
-
-            // var items = new List<TreeViewItemData<string>>(110);
-            // for (var i = 0; i < 10; i++)
-            // {
-            //     var itemIndex = i * 10 + i;
-
-            //     var treeViewSubItemsData = new List<TreeViewItemData<string>>(10);
-
-            //     for (var j = 0; j < 10; j++)
-            //         treeViewSubItemsData.Add(new TreeViewItemData<string>(itemIndex + j + 1, (j+1).ToString()));
-
-            //     var treeViewItemData = new TreeViewItemData<string>(itemIndex, (i+1).ToString(), treeViewSubItemsData);
-            //     items.Add(treeViewItemData);
-            // };
-
-            List<TreeViewItemData<string>> items = new List<TreeViewItemData<string>>();
-
-
-            for (int i = 0; i < SkillModel.ranks.Count; i++)
-            {
-                int itemIndex = i * 100;
-
-                List<TreeViewItemData<string>> subData = new List<TreeViewItemData<string>>(100);
-
-                for (int j = 0; j < skills[index].stats.Count; j++)
-                {
-                    SkillStatModel stat = skills[index].stats[j];
-                    string s_stat = stat.values[i].ToString();
-
-                    foreach (SkillStatTypeModel statType in skillStatTypes)
-                    {
-                        if (statType.ID == stat.statID)
-                        {
-                            s_stat = statType.name + " " + s_stat;
-                            break;
-                        }
-                    }
-
-                    subData.Add(new TreeViewItemData<string>(itemIndex + j + 1, s_stat));
-                }
-
-                items.Add(new TreeViewItemData<string>(itemIndex, SkillModel.ranks[i], subData));
-            }
-
-            TreeView selectedRanks = rootVisualElement.Q<TreeView>("selectedRanks");
-            selectedRanks.SetRootItems(items);
-            selectedRanks.makeItem = () => new Label();
-            selectedRanks.bindItem = (item, index) =>
-            {
-                var i = selectedRanks.GetItemDataForIndex<string>(index);
-                (item as Label).text = i;
-            };
-
-            selectedRanks.Rebuild();
+            return;
         }
+
+        int index = enumerator.Current;
+        Button selectedIcon = rootVisualElement.Q<Button>("selectedIcon");
+        selectedIcon.style.backgroundImage = new StyleBackground(skills[index].icon);
+        //selectedIcon.clicked += ChangeIcon;
+
+        TextField selectedName = rootVisualElement.Q<TextField>("selectedName");
+        selectedName.value = skills[index].name;
+
+        DropdownField selectedCategory = rootVisualElement.Q<DropdownField>("selectedCategory");
+        List<string> names = new List<string>();
+
+        foreach (SkillTypeModel category in skillTypes)
+        {
+            names.Add(category.name);
+
+            if (category.ID == skills[index].categoryID)
+            {
+                selectedCategory.value = category.name;
+            }
+        }
+
+        selectedCategory.choices = names;
+
+        TextField selectedDescription = rootVisualElement.Q<TextField>("selectedDescription");
+        selectedDescription.value = skills[index].description;
+
+        TextField selectedDetails = rootVisualElement.Q<TextField>("selectedDetails");
+        selectedDetails.value = skills[index].details;
+
+        DropdownField selectedFirstRank = rootVisualElement.Q<DropdownField>("selectedFirstRank");
+        selectedFirstRank.value = skills[index].firstAvailableRank;
+        selectedFirstRank.choices = SkillModel.ranks;
+
+        DropdownField selectedStartRank = rootVisualElement.Q<DropdownField>("selectedStartRank");
+        selectedStartRank.value = skills[index].startingRank;
+        selectedStartRank.choices = SkillModel.ranks;       
+        
+        DropdownField selectedLastRank = rootVisualElement.Q<DropdownField>("selectedLastRank");
+        selectedLastRank.value = skills[index].lastAvailableRank;
+        selectedLastRank.choices = SkillModel.ranks;
+
+        FloatField selectedBaseLoadTime = rootVisualElement.Q<FloatField>("selectedBaseLoadTime");
+        selectedBaseLoadTime.value = skills[index].baseLoadTime;           
+
+        FloatField selectedBaseUseTime = rootVisualElement.Q<FloatField>("selectedBaseUseTime");
+        selectedBaseUseTime.value = skills[index].baseLoadTime;    
+
+        FloatField selectedBaseCooldownTime = rootVisualElement.Q<FloatField>("selectedBaseCooldownTime");
+        selectedBaseCooldownTime.value = skills[index].baseLoadTime;
+
+        Toggle selectedIsStartingWith = rootVisualElement.Q<Toggle>("selectedIsStartingWith");
+        selectedIsStartingWith.value = skills[index].isStartingWith; 
+
+        Toggle selectedIsLearnable = rootVisualElement.Q<Toggle>("selectedIsLearnable");
+        selectedIsLearnable.value = skills[index].isLearnable; 
+
+        Toggle selectedIsPassive = rootVisualElement.Q<Toggle>("selectedIsPassive");
+        selectedIsPassive.value = skills[index].isPassive;
+
+        // var items = new List<TreeViewItemData<string>>(110);
+        // for (var i = 0; i < 10; i++)
+        // {
+        //     var itemIndex = i * 10 + i;
+
+        //     var treeViewSubItemsData = new List<TreeViewItemData<string>>(10);
+
+        //     for (var j = 0; j < 10; j++)
+        //         treeViewSubItemsData.Add(new TreeViewItemData<string>(itemIndex + j + 1, (j+1).ToString()));
+
+        //     var treeViewItemData = new TreeViewItemData<string>(itemIndex, (i+1).ToString(), treeViewSubItemsData);
+        //     items.Add(treeViewItemData);
+        // };
+
+        // List<TreeViewItemData<string>> items = new List<TreeViewItemData<string>>();
+
+        // for (int i = 0; i < SkillModel.ranks.Count; i++)
+        // {
+        //     int itemIndex = i * 100;
+
+        //     List<TreeViewItemData<string>> subData = new List<TreeViewItemData<string>>(100);
+
+        //     for (int j = 0; j < skills[index].stats.Count; j++)
+        //     {
+        //         SkillStatModel stat = skills[index].stats[j];
+        //         string s_stat = stat.values[i].ToString();
+
+        //         foreach (SkillStatTypeModel statType in skillStatTypes)
+        //         {
+        //             if (statType.ID == stat.statID)
+        //             {
+        //                 s_stat = statType.name + " " + s_stat;
+        //                 break;
+        //             }
+        //         }
+
+        //         subData.Add(new TreeViewItemData<string>(itemIndex + j + 1, s_stat));
+        //     }
+
+        //     items.Add(new TreeViewItemData<string>(itemIndex, SkillModel.ranks[i], subData));
+        // }
+
+        // TreeView selectedRanks = rootVisualElement.Q<TreeView>("selectedRanks");
+        // selectedRanks.SetRootItems(items);
+        // selectedRanks.makeItem = () => new Label();
+        // selectedRanks.bindItem = (item, index) =>
+        // {
+        //     var i = selectedRanks.GetItemDataForIndex<string>(index);
+        //     (item as Label).text = i;
+        // };
+
+        // selectedRanks.Rebuild();
+
+        MultiColumnListView statView = rootVisualElement.Q<MultiColumnListView>("selectedStats");
+        List<SkillStatModel> stats = skills[index].stats;
+        statView.itemsSource = stats;
+        statView.Rebuild();
     }
 
     private void ChangeIcon()
