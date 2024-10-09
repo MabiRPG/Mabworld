5ea239ba5ac4dc21b7b5ba96c6e7db7cfa5b132a
XanderKing <kenneth.lam1997@hotmail.com>
2024-10-09 07:52:34 -0400

    Added upsert function for skill stat models.

--- Assets/Scripts/Database/SkillStatModel.cs
+++ Assets/Scripts/Database/SkillStatModel.cs
@@ -11,11 +11,25 @@ public class SkillStatModel : BaseModel
     public SkillStatModel(DatabaseManager database, int skillID) : base(database)
     {
         this.skillID = skillID;
-        
-        for (int i = 0; i < values.Capacity; i++)
+        tableName = "skill_stat";
+
+        primaryKeys.Add("skill_id");
+        primaryKeys.Add("skill_stat_id");
+
+        fieldMap.Add("skill_id", new ModelFieldReference(this, nameof(skillID)));
+        fieldMap.Add("skill_stat_id", new ModelFieldReference(this, nameof(statID)));
+
+        for (int i = 1; i <= values.Capacity; i++)
         {
+            string hex = i.ToString("X");
+            int valueIndex = values.Capacity - i;
+
+            fieldMap.Add("r" + hex, new ModelFieldReference(this, nameof(values), valueIndex));
             values.Add(0);
         }
+
+        CreateReadQuery();
+        CreateWriteQuery();
     }
 
     public SkillStatModel(DatabaseManager database, int skillID, int statID) : base(database)
@@ -42,18 +56,6 @@ public class SkillStatModel : BaseModel
         CreateReadQuery();
         CreateWriteQuery();
 
-        // readString = @"SELECT * FROM skill_stat 
-        //     WHERE skill_id = @skill_id AND skill_stat_id = @skill_stat_id;";
-
-        DataRow row = ReadRow();
-
-        // Slice the row by length of ranks
-        // converting to string then float and back to array for the value.
-        // values.AddRange(
-        //     row.ItemArray
-        //         .Skip(2)
-        //         .Take(SkillModel.ranks.Count)
-        //         .Select(x => float.Parse(x.ToString()))
-        //         .ToArray());
+        ReadRow();
     }
 }
